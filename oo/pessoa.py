70895c937f594714389837343cb07d8b02169285
Shaaman331 <tarcisio2010souza@gmail.com>
2021-09-20 20:41:08 -0300

    Criado a classe pessoa.

---
+++ oo/pessoa.py
@@ -0,0 +1,17 @@
+'''
+Classes e Objetos - Fundamentos
+
+* A Programação Orientada a Objetos (POO) é ​​um paradigma de programação baseado no conceito de Classes e Objetos.
+
+Classes podem conter dados e código:
+
+    Dados na forma de campos (também chamamos de atributos ou propriedades); e
+    Código, na forma de procedimentos (frequentemente conhecido como métodos).
+
+
+
+'''
+class Pessoa:
+    olhos = 2 #Atributo de classe
+
+    
