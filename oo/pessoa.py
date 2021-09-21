dc6f05f3c236d4b012dcb20f7bc4a7e45e11dcbd
Shaaman331 <tarcisio2010souza@gmail.com>
2021-09-20 21:04:43 -0300

    Criado o método cumprimentar.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,10 +8,22 @@ Classes podem conter dados e código:
     Dados na forma de campos (também chamamos de atributos ou propriedades); e
     Código, na forma de procedimentos (frequentemente conhecido como métodos).
 
+* Uma importante característica dos objetos é que seus próprios métodos podem acessar e frequentemente modificar seus campos 
+de dados: objetos mantém uma referência para si mesmo, o atributo self no Python.
+
+* Na programação orientada à objetos o foco é na criação de objetos que contem tanto os dados quanto as funcionalidades. 
+Em geral, a definição de cada objeto corresponde a algum objeto ou conceito no mundo real e as funções que operam sobre tal 
+objeto correspondem as formas que os objetos reais interagem
 
 
 '''
 class Pessoa:
-    olhos = 2 #Atributo de classe
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
 
-    
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+
